00b321cf7c8b57cf2752554337f3a867e5246704
AndryDeveloper <65411311+AndryDeveloper@users.noreply.github.com>
2023-12-14 18:59:52 +0300

    Fix bug
    Fixed bug with finding foods by ants

--- controller.py
+++ controller.py
@@ -1,10 +1,11 @@
 import numpy as np
 from random import randint, random
+from typing import List
 
 from config import *
 
 
-def generate_new_target(target_types):
+def generate_new_target(target_types: List[int]):
     """
     Generate new target by next(generate_new_target).
     :param target_types: target's types
@@ -20,7 +21,7 @@ def generate_new_target(target_types):
 
 
 class Env:
-    def __init__(self, t):
+    def __init__(self, t: float):
         """
         :param t: time step
         """
@@ -56,14 +57,14 @@ class Env:
 
 
 class Target:
-    def __init__(self, x, y, target_type):
+    def __init__(self, x: float, y: float, target_type: int):
         """
         :param x: start x coordinate
         :param y: start y coordinate
         :param target_type: type of target
         """
         self.pos = np.array([x, y], dtype=np.float32)
-        self.speed = np.array([0., 0.])
+        self.speed = np.array([0., 0.], dtype=np.float32)
         self.target_type = target_type
         self.size = TARGET_START_SIZE
 
@@ -94,28 +95,42 @@ class Queen(Target):
     pass
 
 
+class Shout:
+    def __init__(self, distances: List[int], pos):
+        """
+        :param distances: distances that ant shout
+        :param pos: ant position
+        """
+        self.pos = pos
+        self.distances = distances
+
+
 class Ant:
-    def __init__(self, x, y, target_types):
+    def __init__(self, x: float, y: float, target_types: List[int]):
         """
         :param x: start x
         :param y: start y
         :param target_types: targets' types without 0 - queen target.
         """
         self.pos = np.array([x, y], dtype=np.float32)
+        self.speed = ANT_SPEED * (1 + random() * ANT_SPEED_NOISE)
         angle = 2 * np.pi * random()
         self.direction = np.array([np.cos(angle), np.sin(angle)])
         self.target_generator = generate_new_target(target_types)
         self.current_target = next(self.target_generator)
         self.target_distances = [np.inf for _ in [0] + target_types]
 
-    def step(self, t, targets, shouts):
+    def step(self, t: float, targets: List[Target], shouts):
         """
         Update ant position and shout if ant find target
         :param t: time step
         :param targets: existing targets
         :param shouts: existing shouts
         """
-        self.pos += ANT_SPEED * (1 + random()*ANT_SPEED_NOISE) * self.direction * t
+        random_angle = 2 * (random() - 1) * ANT_DIRECTION_NOISE
+        random_rotate = np.array([[np.cos(random_angle), -np.sin(random_angle)],
+                                  [np.sin(random_angle), np.cos(random_angle)]])
+        self.pos += self.speed * (self.direction.T @ random_rotate).T * t
         self.target_distances = [dist + 1 for dist in self.target_distances]
 
         # self.pos[0] %= WIDTH
@@ -141,29 +156,18 @@ class Ant:
                 self.current_target = next(self.target_generator)
                 self.direction *= -1
                 shouts.append(Shout([dist + SHOUT_DISTANCE for dist in self.target_distances], self.pos))
-                break
         else:
             if random() < CHANCE_TO_SHOUT:
                 shouts.append(Shout([dist + SHOUT_DISTANCE for dist in self.target_distances], self.pos))
 
-    def update_direction(self, shout):
+    def update_direction(self, shout: Shout):
         """
         Update ant direction
         :param shout: shout
         """
-        if (shout.pos != self.pos).all() and np.linalg.norm(shout.pos - self.pos) <= SHOUT_DISTANCE:
+        if (shout.pos != self.pos).any() and np.linalg.norm(shout.pos - self.pos) <= SHOUT_DISTANCE:
             for i, dist in enumerate(shout.distances):
                 if self.target_distances[i] > dist:
                     self.target_distances[i] = dist
                     if self.current_target == i:
                         self.direction = (shout.pos - self.pos) / np.linalg.norm(shout.pos - self.pos)
-
-
-class Shout:
-    def __init__(self, distances, pos):
-        """
-        :param distances: distances that ant shout
-        :param pos: ant position
-        """
-        self.pos = pos
-        self.distances = distances
